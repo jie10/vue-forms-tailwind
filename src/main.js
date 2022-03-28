790d3ddbb8e1c6937df7c77d59f4e25b5a59c777
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-03-28 09:30:32 +0800

    layout update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import TailContainer from './components/layouts/TailContainer.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('tail-container', TailContainer)
+app.mount('#app')
