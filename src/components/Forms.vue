84167a7f8ba7bdc04747670b2d7e4c4a4e36ac49
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-03-29 21:36:40 +0800

    form

---
+++ src/components/Forms.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+        <div class="mt-1">
+            <input
+                type="text"
+                name="name"
+                id="name"
+                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                placeholder="Enter your name"
+            />
+        </div>
+    </div>
+</template>
