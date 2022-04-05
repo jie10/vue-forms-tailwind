900f4d78279fcc83044056979e16ca946ee81a0a
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-04-05 21:05:09 +0800

    tailwind added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'tailwindcss/tailwind.css'
 import TailContainer from './components/layouts/TailContainer.vue'
 
 const app = createApp(App)
